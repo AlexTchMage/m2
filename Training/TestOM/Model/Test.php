807a5a38d4d25235f6f88cb0b903422fc9574176
alt <alt@mirasvit.com>
2018-11-09 12:04:37 +0200

    homework 1

---
+++ Training/TestOM/Model/Test.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Training\TestOM\Model;
+
+class Test
+{
+	private $manager;
+	private $managerFactory​;
+	private $arrayList;
+	private $name;
+	private $number;
+
+	public function __construct(
+		\Training\TestOM\Model\ManagerInterface $manager,
+		$name,
+		int $number,
+		array $arrayList,
+		\Training\TestOM\Model\ManagerInterfaceFactory $managerFactory​
+	) {
+		$this->manager = $manager;
+		$this->name = $name;
+		$this->number = $number;
+		$this->arrayList = $arrayList;
+		$this->managerFactory​ = $managerFactory​;
+	}
+
+	public function log()
+	{
+		print_r(get_class($this->manager));
+		echo '<br>';
+		print_r($this->name);
+		echo '<br>';
+		print_r($this->number);
+		echo '<br>';
+		print_r($this->arrayList);
+		echo '<br>';
+		$newManager = $this->manager->create();
+		print_r('test: '.get_class($newManager));
+		echo '<br>';
+	}
+}
